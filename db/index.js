e7beb727d5c10895e40d27d9923bfb415bf1b020
Stephanie <sandora375@outlook.com>
2022-01-22 22:00:41 -0600

    updated index in db folder

--- db/index.js
+++ db/index.js
@@ -6,27 +6,36 @@ class DB {
         this.connection = connection;
     }
 
-    findAllEmployees(){
-        return this.connection.promise().query(
-            "SELECT * from employee"
-        )
-    }
-
     findAllDepartments(){
         return this.connection.promise().query(
             'SELECT * from department'
         )
     }
-
+        
     findAllRoles(){
         return this.connection.promise().query(
             'SELECT * from roles'
         )
     }
-
+            
+    findAllEmployees(){
+        return this.connection.promise().query(
+            "SELECT * from employee"
+            )
+    }
+    createDepartment(department){
+        return this.connection.promise().query(
+            "INSERT INTO department SET ?", department
+        )
+    }
     createRole(role){
         return this.connection.promise().query(
             "INSERT INTO roles SET ?", role
+        )
+    }
+    createEmployee(employee){
+        return this.connection.promise().query(
+            "INSERT INTO employee SET ?", employee
         )
     }
 
